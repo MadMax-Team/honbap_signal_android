7f2e97e80735083bebd2a5a02dcc785e75a1b6bb
Jungahgo <kwakjungah0605@gmail.com>
2022-01-16 02:23:54 +0900

    first commit

--- app/src/main/java/com/example/HonbabSignal/LookFragment.kt
+++ app/src/main/java/com/example/HonbabSignal/LookFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.HonbabSignal.databinding.FragmentLookBinding
+import android.content.Intent
+import android.widget.Toast
 
 
 class LookFragment : Fragment() {
@@ -18,9 +20,14 @@ class LookFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentLookBinding.inflate(inflater, container, false)
+
+        //lookBtn 누르면 MapActivity로 넘어가기
+        binding.lookBtn.setOnClickListener {
+            //Toast.makeText(activity, "Its toast!", Toast.LENGTH_SHORT).show();
+
+            val intent = Intent(activity, MapActivity::class.java)
+            startActivity(intent)
+        }
         return binding.root
     }
-
-
-
 }
