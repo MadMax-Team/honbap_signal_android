2b2b2a261ec03a96404dbb7a46674f7b043a1747
Cobboong <ddilong0216@naver.com>
2022-01-16 03:03:57 +0900

    "로그인 activity를 추가했습니다"

--- app/src/main/java/com/example/HonbabSignal/HomeFragment.kt
+++ app/src/main/java/com/example/HonbabSignal/HomeFragment.kt
@@ -1,11 +1,13 @@
 package com.example.HonbabSignal
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.HonbabSignal.databinding.FragmentHomeBinding
+import com.example.HonbabSignal.databinding.FragmentLookBinding
 
 
 class HomeFragment : Fragment() {
@@ -15,8 +17,18 @@ class HomeFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
     ): View {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        //home에서 login버튼 누르면 login activity로 넘어갑니다
+        binding.homeLoginTv.setOnClickListener {
+            //Toast.makeText(activity, "Its toast!", Toast.LENGTH_SHORT).show();
+
+            val intent = Intent(activity, LogInActivity::class.java)
+            startActivity(intent)
+        }
         return binding.root
     }
 
