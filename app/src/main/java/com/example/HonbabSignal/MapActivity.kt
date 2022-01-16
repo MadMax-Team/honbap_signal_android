6ae41ef27ad8b761d8edef5afd9f80758195129d
Jungahgo <kwakjungah0605@gmail.com>
2022-01-17 02:42:29 +0900

    MapActivity에서 Map열기 성공

--- app/src/main/java/com/example/HonbabSignal/MapActivity.kt
+++ app/src/main/java/com/example/HonbabSignal/MapActivity.kt
@@ -1,22 +1,57 @@
 package com.example.HonbabSignal
 
 import android.os.Bundle
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.HonbabSignal.databinding.ActivityMainBinding
 import com.example.HonbabSignal.databinding.ActivityMapBinding
+import com.naver.maps.map.MapView
 import com.naver.maps.map.NaverMapSdk
 
 class MapActivity:AppCompatActivity() {
 
     lateinit var binding: ActivityMapBinding
+    lateinit var mapView: MapView
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient("hs74nz38zh")
-
+        setContentView(R.layout.activity_map)
+        mapView = findViewById<MapView>(R.id.map_view)
+        mapView.onCreate(savedInstanceState)
+    }
+    override fun onStart() {
+        super.onStart()
+        mapView.onStart()
     }
 
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
 
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView.onSaveInstanceState(outState)
+    }
 
+    override fun onStop() {
+        super.onStop()
+        mapView.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
 }
