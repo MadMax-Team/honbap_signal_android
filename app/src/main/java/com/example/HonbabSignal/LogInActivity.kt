6ed22237fb34a70ed4d795b93ccf16ac3446ae57
Jungahgo <kwakjungah0605@gmail.com>
2022-01-26 02:33:09 +0900

    kakao login창 열고 회원가입창 이동 (정보 받아오는 건 아직임)

--- app/src/main/java/com/example/HonbabSignal/LogInActivity.kt
+++ app/src/main/java/com/example/HonbabSignal/LogInActivity.kt
@@ -9,8 +9,12 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.HonbabSignal.databinding.ActivityLogInBinding
 import com.kakao.auth.AuthType
+import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.AuthErrorCause
+import com.kakao.sdk.common.model.ClientError
+import com.kakao.sdk.common.model.ClientErrorCause
+import com.kakao.sdk.common.model.KakaoSdkError
 import com.kakao.sdk.common.util.Utility
 import com.kakao.sdk.user.UserApiClient
 
@@ -23,37 +27,10 @@ class LogInActivity : AppCompatActivity() {
         binding = ActivityLogInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // 로그인 정보 확인
-        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
-            if (error != null) {
-                Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
-            }
-            else if (tokenInfo != null) {
-                Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, MainActivity::class.java)
-                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
-                finish()
-            }
-        }
-
-        UserApiClient.instance.logout { error ->
-            if (error != null) {
-                Log.e(TAG, "로그아웃 실패. SDK에서 토큰 삭제됨", error)
-            }
-            else {
-                Log.i(TAG, "로그아웃 성공. SDK에서 토큰 삭제됨")
-            }
-        }
-
-        //home에서 회원가입 버튼 누르면 signup activity로 넘어갑니다
-        binding.logInSignUpTv.setOnClickListener{
-            startActivity(Intent(this@LogInActivity,SignUpActivity::class.java))
-        }
-
-
         //키 해시 구하기
-        //val keyHash = Utility.getKeyHash(this)
-        //Log.d("Hash", keyHash)
+        val keyHash = Utility.getKeyHash(this)
+        Log.d("Hash", keyHash)
+
 
         val callback: (OAuthToken?, Throwable?) -> Unit = { token, error ->
             if (error != null) {
@@ -89,21 +66,51 @@ class LogInActivity : AppCompatActivity() {
             }
             else if (token != null) {
                 Toast.makeText(this, "로그인에 성공하였습니다.", Toast.LENGTH_SHORT).show()
-                val intent = Intent(this, MainActivity::class.java)
+                val loginToken = token.accessToken
+                Log.d("LoginToken", loginToken)
+                val intent = Intent(this,SignUpActivity::class.java)
                 startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
-                finish()
             }
         }
 
-
-        val kakao_login_button = findViewById<ImageButton>(R.id.log_in_kakao_login_button) // 로그인 버튼
-        kakao_login_button.setOnClickListener {
-
-            if(LoginClient.instance.isKakaoTalkLoginAvailable(this)){
-                LoginClient.instance.loginWithKakaoTalk(this, callback = callback)
-            }else{
-                LoginClient.instance.loginWithKakaoAccount(this, callback = callback)
+        // 카카오 로그인 버튼
+        binding.logInKakaoLoginButton.setOnClickListener {
+            // 카카오 앱이 있는지 판별
+            if (UserApiClient.instance.isKakaoTalkLoginAvailable(this)) {
+                // 있는 경우 앱을 사용하여 로그인
+                UserApiClient.instance.loginWithKakaoTalk(this, callback = callback)
+            } else {
+                // 없을 경우 웹 페이지를 띄움
+                UserApiClient.instance.loginWithKakaoAccount(this, callback = callback)
             }
         }
-    }
+
+        //로그인 상태 유지
+        UserApiClient.instance.accessTokenInfo { tokenInfo, error ->
+            if (error != null) {
+                Toast.makeText(this, "토큰 정보 보기 실패", Toast.LENGTH_SHORT).show()
+            }
+            else if (tokenInfo != null) {
+                Toast.makeText(this, "토큰 정보 보기 성공", Toast.LENGTH_SHORT).show()
+
+                //Log.d("LoginToken", loginToken)
+                //val intent = Intent(this, MainActivity::class.java)
+                //startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP))
+            }
+        }
+
+        UserApiClient.instance.unlink { error ->
+            if (error != null) {
+                Log.e("Logout", "연결 끊기 실패", error)
+            } else {
+                // 연결 끊기 성공 한뒤 작업 부분
+            }
+        }
+
+        //회원가입 버튼 누르면 signup activity로 넘어갑니다
+        binding.logInSignUpTv.setOnClickListener{
+            startActivity(Intent(this@LogInActivity,SignUpActivity::class.java))
+        }
+
+    }//onCreate꺼임
 }
