c84df766bd764cd979fc4a85907c3eac806010f4
Jungahgo <kwakjungah0605@gmail.com>
2022-01-29 04:17:40 +0900

    dm room 열리지만 채팅은 뜨지않음

--- app/src/main/java/com/example/HonbabSignal/DmRoomAdapter.kt
+++ app/src/main/java/com/example/HonbabSignal/DmRoomAdapter.kt
@@ -1,18 +1,16 @@
 package com.example.HonbabSignal
 
+import android.content.ClipDescription
 import android.content.Context
 import android.content.SharedPreferences
-import android.graphics.Bitmap
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.example.HonbabSignal.databinding.ItemMyDmBinding
-import java.net.URL
+import com.example.HonbabSignal.databinding.ItemYourDmBinding
 import java.util.ArrayList
 
 class DmRoomAdapter(val context: Context, val arrayList: ArrayList<DmModel>)
@@ -21,9 +19,18 @@ class DmRoomAdapter(val context: Context, val arrayList: ArrayList<DmModel>)
 
     //internal lateinit var preferences: SharedPreferences
 
+    //클릭 인터페이스 정의
+    interface MyitemClickListener{
+        fun onItemClick(dmModel: DmModel)
+    }
+
+    //리스너 객체를 전달받는 함수랑 리스너 객체를 저장할 변수
+    private lateinit var mItemClickListner: DmRoomAdapter.MyitemClickListener
+
     fun addItem(item: DmModel) {//아이템 추가
         if (arrayList != null) {
             arrayList.add(item)
+            Toast.makeText(this.context, item.name, Toast.LENGTH_SHORT).show()
         }
     }
 
@@ -31,37 +38,56 @@ class DmRoomAdapter(val context: Context, val arrayList: ArrayList<DmModel>)
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         val view: View
         //getItemViewType 에서 뷰타입 1을 리턴받았다면 내채팅레이아웃을 받은 Holder를 리턴
-        val v = viewType
-        Log.d("say",v.toString())
+        //val v = viewType
+        Log.d("say","I'm in onCreateViewHolder")
+
         if(viewType == 1){
-            view = LayoutInflater.from(context).inflate(R.layout.item_my_dm, viewGroup, false)
-            return Holder1(view)
+            //view = LayoutInflater.from(context).inflate(R.layout.item_my_dm, viewGroup, false)
+            val binding: ItemMyDmBinding = ItemMyDmBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+            return Holder1(binding)
         }
         //getItemViewType 에서 뷰타입 2을 리턴받았다면 상대채팅레이아웃을 받은 Holder2를 리턴
         else{
-            view = LayoutInflater.from(context).inflate(R.layout.item_your_dm, viewGroup, false)
-            return Holder2(view)
+            //view = LayoutInflater.from(context).inflate(R.layout.item_your_dm, viewGroup, false)
+            val binding: ItemYourDmBinding = ItemYourDmBinding.inflate(LayoutInflater.from(viewGroup.context), viewGroup, false)
+            return Holder2(binding)
+        }
+    }
+
+    override fun onBindViewHolder(viewHolder:  RecyclerView.ViewHolder, position: Int) {
+        if (viewHolder is Holder1) {
+
+            viewHolder.bind(arrayList[position])
+            //(viewHolder as Holder1).dm_Text?.setText(arrayList.get(position).script)
+            //(viewHolder as Holder1).dm_Time?.setText(arrayList.get(position).date_time)
+
+            //adapter 내부에서 클릭 시 반응하면 외부에선 떨어짐
+            //그래서 interface 사용함
+            viewHolder.itemView.setOnClickListener {mItemClickListner.onItemClick(arrayList[position])}
+
+        }
+        else if(viewHolder is Holder2) {
+            viewHolder.bind(arrayList[position])
+            viewHolder.itemView.setOnClickListener {mItemClickListner.onItemClick(arrayList[position])}
         }
     }
 
     override fun getItemCount(): Int = arrayList.size
 
     //내가친 채팅 뷰홀더
-    inner class Holder1(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-        //친구목록 모델의 변수들 정의하는부분
-        val dm_Text = itemView?.findViewById<View>(R.id.dm_Text)
-        //val dm_Time = itemView?.findViewById(R.id.dm_Time)
+    inner class Holder1(val binding: ItemMyDmBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(dmModel:DmModel){
+            binding.itemMyDmMeName.text = dmModel.script
+        }
     }
 
     //상대가 친 채팅 뷰홀더
-    inner class Holder2(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class Holder2(val binding: ItemYourDmBinding) : RecyclerView.ViewHolder(binding.root) {
 
-        //친구목록 모델의 변수들 정의하는부분
-        val dm_You_Image = itemView?.findViewById<View>(R.id.dm_You_Image)
-        val dm_You_Name = itemView?.findViewById<View>(R.id.dm_You_Name)
-        val dm_Text = itemView?.findViewById<View>(R.id.dm_Text)
-        //val dm_Time = itemView?.findViewById(R.id.dm_Time)
+        fun bind(dmModel: DmModel){
+            binding.itemYourDmText.text = dmModel.script
+            binding.itemYourDmYouName.text = dmModel.name
+        }
 
     }
 
@@ -84,19 +110,5 @@ class DmRoomAdapter(val context: Context, val arrayList: ArrayList<DmModel>)
         }
     }
 
-    override fun onBindViewHolder(viewHolder:  RecyclerView.ViewHolder, position: Int) {
-        //onCreateViewHolder에서 리턴받은 뷰홀더가 Holder라면 내채팅, item_my_chat의 뷰들을 초기화 해줌
-        if (viewHolder is Holder1) {
 
-            //(viewHolder as Holder1).dm_Text?.setText(arrayList.get(position).script)
-            //(holder as Holder1).dm_Time?.setText(arrayList.get(position).date_time)
-        }
-        //onCreateViewHolder에서 리턴받은 뷰홀더가 Holder2라면 상대의 채팅, item_your_chat의 뷰들을 초기화 해줌
-        else if(viewHolder is Holder2) {
-            //(viewHolder as Holder2).dm_You_Image?.setImageResource(R.mipmap.ic_launcher)
-            //(viewHolder as Holder2).dm_You_Name?.setText(arrayList.get(position).name)
-            //(viewHolder as Holder2).dm_Text?.setText(arrayList.get(position).script)
-            //(holder as Holder2).dm_Time?.setText(arrayList.get(position).date_time)
-        }
-    }
 }
