66109e1aa829b8e77bac62cffd12765f9c0040e4
Cobboong <ddilong0216@naver.com>
2022-01-29 04:13:31 +0900

    이전과 다르게 그냥 만들어서 조건 확인만 하고 서버로 정보 넘기는 signUp 프로토타입 생성완료

--- app/src/main/java/com/example/HonbabSignal/AuthResponse.kt
+++ app/src/main/java/com/example/HonbabSignal/AuthResponse.kt
@@ -1,3 +1,4 @@
 package com.example.HonbabSignal
 
+//output을 정의하는 곳
 data class AuthResponse(val isSuccess : Boolean, val code: Int, val message : String)
--- app/src/main/java/com/example/HonbabSignal/SignUpService.kt
+++ app/src/main/java/com/example/HonbabSignal/SignUpService.kt
@@ -3,11 +3,15 @@ package com.example.HonbabSignal
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
 
+
 interface SignUpService {
+    @FormUrlEncoded
     @POST("/user/signup")
     fun signUp(
+        //input을 정의하는곳
         @Field("userId") userId:String,
         @Field("password") password:String,
         @Field("userName") userName:String,
