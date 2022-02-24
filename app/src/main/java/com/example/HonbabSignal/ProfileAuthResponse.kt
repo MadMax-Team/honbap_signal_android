286cff4c9e3fa73300f830532193bc5850036b12
Jungahgo <kwakjungah0605@gmail.com>
2022-02-24 22:35:41 +0900

    EditingProfileActivity all spinner 장착

---
+++ app/src/main/java/com/example/HonbabSignal/ProfileAuthResponse.kt
@@ -0,0 +1,24 @@
+package com.example.HonbabSignal
+
+import com.google.gson.annotations.SerializedName
+
+data class AuthProfileImg(@SerializedName("type") val type: String,
+                          @SerializedName("data") val data: List<String>)
+
+data class AuthProfile(@SerializedName("nickName") val nickName : String,
+                       @SerializedName("profileImg") val profileImg: List<AuthProfileImg>,
+                       @SerializedName("taste") val taste: String,
+                       @SerializedName("hateFood") val hateFood: String,
+                       @SerializedName("interest") val interest: String,
+                       @SerializedName("avgSpeed") val avgSpeed: String,
+                       @SerializedName("preferArea") val preferArea: String,
+                       @SerializedName("mbti") val mbti: String,
+                       @SerializedName("userIntroduce") val userIntroduce: String,
+                       @SerializedName("updateAt") val updateAt: String)
+
+//userIdx GET의 output을 정의
+data class ProfileAuthResponse(@SerializedName("isSuccess") val isSuccess: Boolean,
+                               @SerializedName("code") val code: Int,
+                               @SerializedName("message") val message: String,
+                               @SerializedName("result") val result : List<AuthProfile>
+                               )
