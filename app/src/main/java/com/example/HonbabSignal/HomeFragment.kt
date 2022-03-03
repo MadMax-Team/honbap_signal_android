475498934411c0fc0909338bc84ae7b32b6cb51c
Jungahgo <kwakjungah0605@gmail.com>
2022-03-03 21:46:10 +0900

    HomeFragment add signal api post -ing

--- app/src/main/java/com/example/HonbabSignal/HomeFragment.kt
+++ app/src/main/java/com/example/HonbabSignal/HomeFragment.kt
@@ -1,13 +1,17 @@
 package com.example.HonbabSignal
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import retrofit2.Call
+import retrofit2.Callback
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.HonbabSignal.databinding.FragmentHomeBinding
-
+import retrofit2.Response
 
 
 class HomeFragment : Fragment() {
@@ -27,6 +31,8 @@ class HomeFragment : Fragment() {
             binding.homeAfterSignalOnIv.visibility = View.VISIBLE
             binding.homeAfterLoginSignalToMeLl.visibility = View.VISIBLE
             binding.homeAfterLoginDmToMeLl.visibility = View.VISIBLE
+
+
         }
         binding.homeAfterSignalOnIv.setOnClickListener {
             binding.homeAfterSignalOnIv.visibility = View.GONE
@@ -43,7 +49,34 @@ class HomeFragment : Fragment() {
 
         }
 
+        fun retrofitPostSignal(){
+            var retrofit = getRetorfit()
+            var SignalService = retrofit.create(SignalService::class.java)
+            var userIdx: Int = 1
+            var signalIdx: String = "1"
+            var applyIdx: String = "1"
 
+            SignalService.addSignal(userIdx, signalIdx, applyIdx )
+                .enqueue(object: Callback<SignalResponse>{
+                    override fun onResponse(
+                        call: Call<SignalResponse>,
+                        response: Response<SignalResponse>
+                    ) {
+
+                        var respIdx = response.body()!!
+                        when (respIdx.code){
+                            1000 -> {
+                                Log.d("HomeFragment",respIdx.code.toString())
+                            }
+
+                        }
+                    }
+
+                    override fun onFailure(call: Call<SignalResponse>, t: Throwable) {
+                        Log.d("HomeFragment", "signal add onFailure")
+                    }
+                })
+        }
 
 
         return binding.root
