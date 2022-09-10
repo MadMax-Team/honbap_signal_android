9f879fc59612898372c1a358ee292ed81ed59a9d
곽정아 <goak@Goak-MAC.local>
2022-09-11 03:46:52 +0900

    dmList code 정리

--- app/src/main/java/com/example/HonbabSignal/DM/DmListFragment.kt
+++ app/src/main/java/com/example/HonbabSignal/DM/DmListFragment.kt
@@ -1,14 +1,12 @@
 package com.example.HonbabSignal.DM
 
 import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.HonbabSignal.R
 import com.example.HonbabSignal.RetrofitSevices.SignalService
 import com.example.HonbabSignal.databinding.FragmentDmListBinding
 import com.example.HonbabSignal.dmRoomListResponse
@@ -20,13 +18,16 @@ import retrofit2.Response
 
 class DmListFragment : Fragment(){
     lateinit var binding: FragmentDmListBinding
-    private var dmRoomListDatas = ArrayList<Friend>()
+    private var dmRoomListDatas = ArrayList<DmRoom>()
     private lateinit var dmRoomId : String
     private lateinit var jwt: String
 
-    private fun getDmRoomList(jwt: String, dmRoomListDatas: ArrayList<Friend>){
-        var retrofit = getRetorfit()
-        var signalService = retrofit.create(SignalService::class.java)
+    var retrofit = getRetorfit()
+    var signalService = retrofit.create(SignalService::class.java)
+
+    //DmRoom GET API로 dmRoom연결
+    //세부정보 추가필요
+    private fun getDmRoomList(jwt: String, dmRoomListDatas: ArrayList<DmRoom>){
 
         signalService.getDmRoomList(jwt)
             .enqueue((object: Callback<dmRoomListResponse>{
@@ -42,7 +43,7 @@ class DmListFragment : Fragment(){
 
                                 dmRoomListDatas.apply{
                                     add(
-                                        Friend("name",null,"last",i.roomId)
+                                        DmRoom("name",null,"last",null,i.roomId)
                                     )
                                 }
                             }
@@ -60,7 +61,8 @@ class DmListFragment : Fragment(){
             }))
     }
 
-    private fun setAdapter(dmRoomListDatas: ArrayList<Friend>){
+    //Adapter setting
+    private fun setAdapter(dmRoomListDatas: ArrayList<DmRoom>){
         // 더미데이터랑 adapter 연결
         val dmListAdapter = DmListAdapter(dmRoomListDatas)
         // 리사이클러뷰에 adapter 연결
@@ -74,11 +76,13 @@ class DmListFragment : Fragment(){
     ): View {
         binding = FragmentDmListBinding.inflate(inflater, container, false)
 
+        //jwt
         val spf_jwt = this.getActivity()?.getSharedPreferences("jwt", Context.MODE_PRIVATE)
         jwt = spf_jwt?.getString("jwt", "").toString()
         Log.d("jwt", jwt)
 
        getDmRoomList(jwt,dmRoomListDatas)
+
         return binding.root
     }
 }
